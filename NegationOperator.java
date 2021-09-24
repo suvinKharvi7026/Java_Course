d7489b8852dcc80ee4293bf7abd9f9b711a34acc
Suvin kharvi <suvin.kharvi789@gmail.com>
2021-09-24 07:16:24 +0530

    writes NegationOperator program

---
+++ NegationOperator.java
@@ -0,0 +1,9 @@
+class NegationOperator{
+public static void main(String[] args){
+System.out.println("Preincrement operation........");
+boolean s = false;
+boolean result = !s;
+System.out.println("s :" +s);
+System.out.println("result :" +result);
+}
+}
