912914b26a12580a7181604d9bb3d2717ea3ac97
Suvin kharvi <suvin.kharvi789@gmail.com>
2021-09-24 07:12:30 +0530

    writes IncrementOperator program

---
+++ IncrementOperator.java
@@ -0,0 +1,14 @@
+class IncrementOperator{
+public static void main(String[] args){
+System.out.println("Preincrement operation...");
+int a = 60;
+int b = ++a;
+int i = 60;
+int j = i++;
+System.out.println("a :" +a);
+System.out.println("b :" +b);
+System.out.println("Postincrement operation...");
+System.out.println("i :" +i);
+System.out.println("j :" +j);
+}
+}
