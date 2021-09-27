055aa46cde7569ff51e3a9e44d465dc6b259ab2e
suvinKharvi7026 <91167312+suvinKharvi7026@users.noreply.github.com>
2021-09-27 07:31:31 +0530

    writes BitwiseOperatorXorAssignment Program

---
+++ BitwiseOperatorXorAssignment.java
@@ -0,0 +1,11 @@
+class BitwiseOperatorXorAssignment{
+public static void main(String[] args){
+int x=6;
+int y=9;
+System.out.println(x<y ^ x<=y);
+System.out.println(x<y ^ x>y);
+System.out.println(x==y ^ x<=y);
+System.out.println(x<y ^ x>=y);
+System.out.println(x>y ^ x>=y);
+}
+}
