606b9243d4444c079492b705030bfb4a378d1a17
suvinKharvi7026 <91167312+suvinKharvi7026@users.noreply.github.com>
2021-09-27 07:34:18 +0530

    writes RelationalOperatorAssignment program

---
+++ RelationalOperatorAssignment.java
@@ -0,0 +1,29 @@
+class RelationalOperatorAssignment{
+public static void main(String[] args){
+int m=90;
+int n=50;
+int o=75;
+boolean result=m<n;
+System.out.println("m<n:"+result);
+boolean result1=n<m;
+System.out.println("n<m:"+result1);
+boolean result3=m>n;
+System.out.println("m>n:"+result3);
+boolean result4=n>m;
+System.out.println("n>m:"+result4);
+boolean result5=m<=o;
+System.out.println("m<=o:"+result5);
+boolean result6=m>=n;
+System.out.println("m>=n:"+result6);
+boolean result7=m==o;
+System.out.println("m==o:"+result7);
+boolean result8=m!=o;
+System.out.println("m!=o:"+result8);
+
+boolean result9=n!=o;
+System.out.println("n!=o:"+result9);
+boolean result10=n==o;
+System.out.println("n==o:"+result10);
+
+}
+}
