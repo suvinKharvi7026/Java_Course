bd6544bc0b16ba6dddfddabe26a5e641c5c975a6
suvinKharvi7026 <91167312+suvinKharvi7026@users.noreply.github.com>
2021-09-27 07:26:19 +0530

    Add files via upload

---
+++ AssignmentOperator2.java
@@ -0,0 +1,12 @@
+class AssignmentOperator2{
+public static void main(String[] args){
+int x=85;
+int y=45;
+x-=y;
+System.out.println("x:"+x);
+int i=60;
+int j=5;
+i*=j;
+System.out.println("i:"+i);
+}
+}
