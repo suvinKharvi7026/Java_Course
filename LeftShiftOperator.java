14903a9af6e96e9171a2ed93394b41bf895d006b
suvinKharvi7026 <91167312+suvinKharvi7026@users.noreply.github.com>
2021-09-28 11:23:54 +0530

    writes LeftShiftOperator  program

---
+++ LeftShiftOperator.java
@@ -0,0 +1,12 @@
+class LeftShiftOperator{
+public static void main(String[] args){
+      
+       
+       int a=20;
+       int b=a<<2;
+       System.out.println(b);
+       System.out.println("It is Left Shift Operator.");
+       
+      
+}
+}
