62a4d10b8dfb632f6114c0452826e11836d3c424
suvinKharvi7026 <91167312+suvinKharvi7026@users.noreply.github.com>
2021-09-28 11:24:46 +0530

    writes RightShiftOperator program

---
+++ RightShiftOperator.java
@@ -0,0 +1,12 @@
+class RightShiftOperator{
+public static void main(String[] args){
+      
+       
+       int a=27;
+       int b=a>>3;
+       System.out.println(b);
+       System.out.println("It is Right Shift Operator");
+        
+       
+}
+}
