7828109d8e0dc9634b239663412e62a075f68129
suvinKharvi7026 <91167312+suvinKharvi7026@users.noreply.github.com>
2021-10-12 07:11:45 +0530

    writes Bool program

---
+++ Bool.java
@@ -0,0 +1,13 @@
+public class Bool{
+  public static void main(String[] args){
+   
+    boolean x = true;
+    if(x)
+      System.out.println("x = " + x +"... hello developers");
+    
+   
+    Boolean y = false;
+    if(y.toString().equals("false"))
+      System.out.println("y = " + y +"... hello jokers");
+  }
+}
