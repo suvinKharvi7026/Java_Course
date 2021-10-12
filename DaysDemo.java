aeb2a71e8d38982714b02fb71d01a703ec8cf8d8
suvinKharvi7026 <91167312+suvinKharvi7026@users.noreply.github.com>
2021-10-12 07:01:08 +0530

    Writes DaysDemo program

---
+++ DaysDemo.java
@@ -0,0 +1,15 @@
+class DaysDemo{
+public static void main(String[] args){
+int n=9;
+switch(n){
+  case 1:System.out.println("Monday");break;
+  case 2:System.out.println("Sunday"); break;
+  case 3:System.out.println("Saturday");break;
+  case 4:System.out.println("Friday"); break;
+  case 5:System.out.println("Thursday");break;
+  case 6:System.out.println("Wednesday");break;
+  case 7:System.out.println("Tuesday");break;
+  default:System.out.println("Please enter number between 1 to 7");break;
+}
+}
+}
