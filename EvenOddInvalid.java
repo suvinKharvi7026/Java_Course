8db43f1ace100f12cfe01e13f3deb631a0123321
suvinKharvi7026 <91167312+suvinKharvi7026@users.noreply.github.com>
2021-10-12 07:13:07 +0530

    writes EvenOddInvalid program

---
+++ EvenOddInvalid.java
@@ -0,0 +1,14 @@
+class EvenOddInvalid{
+public static void main(String args[]){
+   long num=125435434;
+
+    
+       if(num >1000000000){
+            System.out.println("it is valid number");
+        }else{
+System.out.println("it is invalid number");
+       
+        }
+    }
+     
+  }  
