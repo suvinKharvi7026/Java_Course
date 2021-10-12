fc81623041a890cdde7dd5b867022a6939974776
suvinKharvi7026 <91167312+suvinKharvi7026@users.noreply.github.com>
2021-10-12 07:00:19 +0530

    writes Long program

---
+++ Long.java
@@ -0,0 +1,14 @@
+class Long{
+public static void main(String args[]){
+   long num=12543543;
+
+    
+       if(num >1000000000){
+            System.out.println("it is valid number");
+        }else{
+System.out.println("it is invalid number");
+       
+        }
+    }
+     
+  }
