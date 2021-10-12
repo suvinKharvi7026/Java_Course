1fe78ec36f49bfd2ae3d481432a7401acd84dc4c
suvinKharvi7026 <91167312+suvinKharvi7026@users.noreply.github.com>
2021-10-12 07:18:55 +0530

    writes LogicalOperatorDemo program

---
+++ LogicalOperatorDemo.java
@@ -0,0 +1,10 @@
+class LogicalOperatorDemo{
+public static voidmain(String[] args){
+int a=10;
+int b=20;
+System.out.println(a<b && a<=b);
+System.out.println(a<b && a>b);
+System.out.println(a==b && a!=b);
+System.out.println(a==b && a>b);
+}
+}
