e829b4159b9178ad1da7eef77cd3046e24ac4379
suvinKharvi7026 <91167312+suvinKharvi7026@users.noreply.github.com>
2021-10-12 07:19:30 +0530

    writes LogicalOperatorAssignment program

---
+++ LogicalOperatorAssignment.java
@@ -0,0 +1,10 @@
+class LogicalOperatorAssignment{
+public static void main(String[] args){
+int m=40;
+int n=50;
+System.out.println(m<n && m<=n);
+System.out.println(m<n && m>n);
+System.out.println(m==n && m!=n);
+System.out.println(m==n && m>n);
+}
+}
