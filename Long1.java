1abb5134ef054a84d9a8bb540f11f7843affc047
suvinKharvi7026 <91167312+suvinKharvi7026@users.noreply.github.com>
2021-10-12 06:58:01 +0530

    Writes Long1 program

---
+++ Long1.java
@@ -0,0 +1,14 @@
+class Long1{
+public static void main(String args[]){
+   long num=1254354321;
+
+    
+       if(num >1000000000){
+            System.out.println("it is valid number");
+        }else{
+System.out.println("it is invalid number");
+       
+        }
+    }
+     
+  }
