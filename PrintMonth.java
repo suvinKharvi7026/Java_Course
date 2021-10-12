dd6e40ad5e17d7ca83bbe1303daf02bb1ab730ce
suvinKharvi7026 <91167312+suvinKharvi7026@users.noreply.github.com>
2021-10-12 07:07:17 +0530

    writes PrintMonth program

---
+++ PrintMonth.java
@@ -0,0 +1,46 @@
+class PrintMonth{
+public static void main(String[] args){
+int n=14;
+if(n>0&&n<=12){
+  if(n==1){
+  System.out.println("January");
+  }
+  else if(n==2){
+  System.out.println("February");
+  }
+  else if(n==3){
+  System.out.println("March");
+  }
+  else if(n==4){
+  System.out.println("April");
+  }
+  else if(n==5){
+  System.out.println("May");
+  }
+  else if(n==6){
+  System.out.println("June");
+  }
+  else if(n==7){
+  System.out.println("July");
+  }
+  else if(n==8){
+  System.out.println("August");
+  }
+  else if(n==9){
+  System.out.println("September");
+  }
+  else if(n==10){
+  System.out.println("October");
+  }
+  else if(n==11){
+  System.out.println("November");
+  }
+  else if(n==12){
+  System.out.println("December");
+  }
+}
+else{
+  System.out.println("Please enter number between 1 to 12");
+}
+}
+}
