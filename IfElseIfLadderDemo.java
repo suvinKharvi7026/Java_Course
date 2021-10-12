f96458e69be57f4b13c8a9405027f06cfe534abe
suvinKharvi7026 <91167312+suvinKharvi7026@users.noreply.github.com>
2021-10-12 06:56:03 +0530

    writes IfElseIfLadderDemo program

---
+++ IfElseIfLadderDemo.java
@@ -0,0 +1,19 @@
+class IfElseIfLadderDemo{
+public static void main(String[] args){
+boolean aadhar=true;
+boolean panCard=false;
+boolean voterId=false;
+if(voterId){
+System.out.println("you are an indian citizen");
+}
+else if(voterId){
+System.out.println("you are an indian citizen");
+}
+else if(aadhar){
+System.out.println("you are an indian citizen");
+}
+else {
+System.out.println("you are n0t an indian citizen");
+}
+}
+}
