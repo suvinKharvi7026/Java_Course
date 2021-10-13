6275912e37a9ace3abb6bc7854d2a49c3cd402eb
suvinKharvi7026 <91167312+suvinKharvi7026@users.noreply.github.com>
2021-10-13 07:26:55 +0530

    writes NestedFor program

---
+++ NestedFor.java
@@ -0,0 +1,10 @@
+class NestedFor{
+public static void main(String[] args){
+        for(int i=0;i<3;i++){
+          for(int j=0;j<3;j++){
+               System.out.print(j+"");
+          }
+        System.out.println();
+        }
+}
+}
