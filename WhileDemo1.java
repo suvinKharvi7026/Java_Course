ce8b4e86cf34b5a4b5750729aacfed43b9aba45d
suvinKharvi7026 <91167312+suvinKharvi7026@users.noreply.github.com>
2021-10-13 07:28:28 +0530

    writes WhileDemo1 program

---
+++ WhileDemo1.java
@@ -0,0 +1,9 @@
+class WhileDemo1{
+public static void main(String[] args){
+           int age=18;
+            while(age==18){
+             System.out.println("YOUR AGE IS 18");
+             age++;
+            }
+}
+}
