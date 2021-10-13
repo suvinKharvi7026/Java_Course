c435d4a090a628cd333baa73ee28575cdd26e128
suvinKharvi7026 <91167312+suvinKharvi7026@users.noreply.github.com>
2021-10-13 07:29:13 +0530

    writes WhileDemo2 program

---
+++ WhileDemo2.java
@@ -0,0 +1,9 @@
+class WhileDemo2{
+public static void main(String[] args){
+            int count=0;
+            while(count<4){
+            System.out.println(count);
+            count++;
+}
+}
+}
