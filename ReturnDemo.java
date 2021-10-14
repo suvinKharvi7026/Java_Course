6f425cec0a9a9d40523dcfe56684de164c31d3b8
suvinKharvi7026 <91167312+suvinKharvi7026@users.noreply.github.com>
2021-10-14 07:02:36 +0530

    Writes ReturnDemo program

---
+++ ReturnDemo.java
@@ -0,0 +1,15 @@
+class ReturnDemo{
+
+
+         static int squareRoot(int number){
+         int squar=number*number;
+         return squar;
+
+}
+public static void main(String[] args){
+      int square=squareRoot(10);
+      System.out.println("square:"+square);
+
+
+}
+}
